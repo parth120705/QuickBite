89f42beb65a2de444b170bc203bb5ae1878d48ca
parth120705 <munjalparth123@gmail.com>
2026-03-03 23:04:02 +0530

    google auth setup backend

---
+++ services/auth/dist/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoute from './routes/auth.js';
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoute);
+app.listen(PORT, () => {
+    console.log(`Auth service is running at port ${PORT}`);
+    connectDB();
+});
